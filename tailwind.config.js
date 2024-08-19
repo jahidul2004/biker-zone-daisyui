5ef67ea26082b5e7e4d8a1f179266e0c76bb1973
jahidul2004 <islamjahiduljihad@gmail.com>
2024-08-19 21:40:53 +0600

    Carousel section added.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
       extend: {
-        colors: {
-            'bike-primary' : '#E76F51'
-        }
       },
     },
     plugins: [],
