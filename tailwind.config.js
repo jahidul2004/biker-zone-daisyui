1e5d8e8d1e651575487576b90335054b1b6bc346
jahidul2004 <islamjahiduljihad@gmail.com>
2024-08-19 20:40:03 +0600

    Navbar section added,

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+            'bike-primary' : '#E76F51'
+        }
+      },
     },
     plugins: [],
   }
